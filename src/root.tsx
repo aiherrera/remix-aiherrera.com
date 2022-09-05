24b21f19e15865a62b449f3da12a5f94379b94d8
Alain Iglesias <alain.iherrera@gmail.com>
2022-09-05 15:17:40 -0500

    chore: add scripts and config font & reset styles

--- src/root.tsx
+++ src/root.tsx
@@ -1,22 +1,34 @@
-import type { MetaFunction } from "@remix-run/node";
+// Libraries
+import type { LinksFunction, MetaFunction } from '@remix-run/node'
 import {
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+  ScrollRestoration
+} from '@remix-run/react'
+
+// Styles
+import AsapFontStyle from '@fontsource/asap/index.css'
+import remixImageStyles from 'remix-image/remix-image.css'
+import normalize from './styles/normalize.css'
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+  charset: 'utf-8',
+  title: 'aiherrera | Personal website',
+  viewport: 'width=device-width,initial-scale=1'
+})
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: AsapFontStyle },
+  { rel: 'stylesheet', href: normalize },
+  { rel: 'stylesheet', href: remixImageStyles }
+]
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
         <Meta />
         <Links />
@@ -28,5 +40,5 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
